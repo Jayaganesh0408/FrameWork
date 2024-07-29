8e432476b0ac2aab2f4ba4b7e73cb6f42390513a
Jayaganesh <jayaganeshponpandian@gmail.com>
2024-07-29 11:28:19 +0530

    second

--- Automation2/src/test/java/cucumber/TestNGTestRunner.java
+++ Automation2/src/test/java/cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/cucumber", glue="stepDefinitions", monochrome=true,
-/*tags="@Smoke",*/plugin= {"html:target/cucumber.html"})
+tags="@Smoke",plugin= {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 
 }
